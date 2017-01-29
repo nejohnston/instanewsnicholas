c082c6f620689324f1e4c5af4a2f2231546077f6
njohnstud <n.e.johnston@hotmail.com>
2017-01-29 01:15:40 -0800

    Sliced and diced data, also filtered it

--- js/main.js
+++ js/main.js
@@ -14,13 +14,16 @@ $.ajax({
   method: 'GET',
 })
 .done(function(data) {
-   function bouncer(data) {
-  function filterer(arr) {
-     return arr > 0|| isNaN(arr) === true;
-  }
-  arr = arr.filter(filterer);
-  return arr;
-}
+   var newsArticles = data.results;
+
+ function newsWithArticles(newsArticles){
+  return newsArticles.multimedia.length === 5;
+ }
+// console.log(dataSet);
+var filteredNews = newsArticles.filter(newsWithArticles)
+// console.log(filteredArray);
+var slicedNews = filteredNews.slice(0,12);
+// console.log(slicedArray);
     // var newsArray = data.results;
     // var newsWithImages = newsArray.multimedia.filter(function(images){
     // return images > 0 });
@@ -28,13 +31,13 @@ $.ajax({
 
     
 var categorySearch = ''; 
-    $.each(newsFiltered, function (key, value) {
+    $.each(slicedNews, function (key, value) {
                         var newsCaption =  value.abstract 
                         var urlArticle = value.url 
                         var urlImage =  '<img class="insta-image" src="' + value.multimedia[4].url + '">'
                         categorySearch += '<li><a href="' + urlArticle + '">'
-                        categorySearch += '<img style="background-image:' + urlImage + '"></a>'
-                        categorySearch += '<div>' + newsCaption + '</div></li>'
+                        categorySearch += '<div style="background-image:' + urlImage + '">'
+                        categorySearch += '<div class="abstract">' + newsCaption + '</div></a></li>'
                         $('.category-search').append(urlImage + newsCaption)
 
  })})
