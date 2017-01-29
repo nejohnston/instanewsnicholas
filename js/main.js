f0a45c1006df7a485f509b3aaf083c721a031949
njohnstud <n.e.johnston@hotmail.com>
2017-01-28 23:41:31 -0800

    Sizing images, working on filter function

--- js/main.js
+++ js/main.js
@@ -4,20 +4,42 @@ $(function() {
 // => 1
 // => Section
 $('select').on('change', function() {
-var category = $('#options').val();
-    
+var category = $('#options').val(); 
 var url = 'http://api.nytimes.com/svc/topstories/v2/' + category + '.json';
 url += '?' + $.param({
   'api-key': 'a631fc3fad2941638962121f9d0dcb1c'
-});
+})
 $.ajax({
   url: url,
   method: 'GET',
-}).done(function(data) {
-    $.each()
-    console.log(data.results);
-}).fail(function(err) {
+})
+.done(function(data) {
+   function bouncer(data) {
+  function filterer(arr) {
+     return arr > 0|| isNaN(arr) === true;
+  }
+  arr = arr.filter(filterer);
+  return arr;
+}
+    // var newsArray = data.results;
+    // var newsWithImages = newsArray.multimedia.filter(function(images){
+    // return images > 0 });
+    // console.log(images)
+
+    
+var categorySearch = ''; 
+    $.each(newsFiltered, function (key, value) {
+                        var newsCaption =  value.abstract 
+                        var urlArticle = value.url 
+                        var urlImage =  '<img class="insta-image" src="' + value.multimedia[4].url + '">'
+                        categorySearch += '<li><a href="' + urlArticle + '">'
+                        categorySearch += '<img style="background-image:' + urlImage + '"></a>'
+                        categorySearch += '<div>' + newsCaption + '</div></li>'
+                        $('.category-search').append(urlImage + newsCaption)
+
+ })})
+.fail(function(err) {
   throw err;
-});
-});
-});
+})
+})
+})
